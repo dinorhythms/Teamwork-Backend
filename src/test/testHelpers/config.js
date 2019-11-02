66fc568edd03b04834ca7048e7207df24cc174c4
Oladehinde Kazeem <larrysnet2001@gmail.com>
2019-11-02 11:18:07 +0100

    chore(mocha test) setup test framework with mocha
    - install mocha, nyc and necessary dependecies
    - create test command in package.json
    - create text config with necessary test dependecies
    - test index.js
    
    [Maintains]

---
+++ src/test/testHelpers/config.js
@@ -0,0 +1,27 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import { mockRequest, mockResponse } from 'mock-req-res';
+import app from '../../index';
+import messages from '../../utils/messages';
+
+// Define the expect assertion
+const { expect } = chai;
+const BACKEND_BASE_URL = '/api/v1';
+
+// chai middleware
+chai.use(chaiHttp);
+chai.use(sinonChai);
+chai.should();
+
+export {
+  app,
+  messages,
+  chai,
+  expect,
+  sinon,
+  mockRequest,
+  mockResponse,
+  BACKEND_BASE_URL,
+};
