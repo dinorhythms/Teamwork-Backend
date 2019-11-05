d001720835f4ce6154a4ead7447f967e60fe1b58
Oladehinde Kazeem <larrysnet2001@gmail.com>
2019-11-05 15:17:58 +0100

    feat(database): create db services and migration
    - install pg and make runnable
    - setup db connection configuration
    - create db services helper
    - create db migration files
    - create migration run command in package.json
    
    [Finishes #card-28752900]

---
+++ src/db/index.js
@@ -0,0 +1,45 @@
+import { Pool } from 'pg';
+
+let DATABASE_URL = null;
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    DATABASE_URL = process.env.DEV_DATABASE_URL;
+    break;
+  case 'production':
+    DATABASE_URL = process.env.DATABASE_URL;
+    break;
+  case 'test':
+    DATABASE_URL = process.env.TEST_DATABASE_URL;
+    break;
+  default:
+    DATABASE_URL = process.env.DEV_DATABASE_URL;
+    break;
+}
+
+const pool = new Pool({
+  connectionString: DATABASE_URL
+});
+
+pool.on('connect', () => {
+  console.log('connected to the Database');
+});
+
+// a generic query, that executes all queries you send to it
+const query = async (text) => new Promise((resolve, reject) => {
+  pool
+    .query(text)
+    .then((res) => {
+      resolve(res);
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
+
+pool.on('remove', () => {
+  console.log('client removed');
+  process.exit(0);
+});
+
+export { query };
---
+++ src/services/dbServcies.js
@@ -0,0 +1,87 @@
+import { query } from '../db';
+
+const getAll = async (table) => {
+  try {
+    const getAllQuery = `SELECT * FROM ${table}`;
+    const { rows, rowCount } = await query(getAllQuery);
+    if (rowCount > 0) {
+      return rows;
+    }
+    return null;
+  } catch (error) {
+    return error;
+  }
+};
+
+const getById = async (table, id) => {
+  try {
+    const getQuery = `SELECT * FROM ${table} WHERE id = ${id}`;
+    const { rows, rowCount } = await query(getQuery);
+    if (rowCount > 0) {
+      return rows[0];
+    }
+    return null;
+  } catch (error) {
+    return error;
+  }
+};
+
+const getAllByOption = async (table, option) => {
+  try {
+    const getOptionQuery = `SELECT * FROM ${table} WHERE ${option}`;
+    const { rows, rowCount } = await query(getOptionQuery);
+    if (rowCount > 0) {
+      return rows;
+    }
+    return null;
+  } catch (error) {
+    return error;
+  }
+};
+
+const getSelectedByOption = async (table, columns, option) => {
+  try {
+    const getOptionQuery = `SELECT ${columns} FROM ${table} WHERE ${option}`;
+    const { rows, rowCount } = await query(getOptionQuery);
+    if (rowCount > 0) {
+      return rows;
+    }
+    return null;
+  } catch (error) {
+    return error;
+  }
+};
+
+const insertRecord = async (table, columns, values) => {
+  try {
+    const insertQuery = `INSERT INTO ${table} ( ${columns} ) VALUES (${values}) RETURNING *`;
+    const { rows } = await query(insertQuery);
+    return rows;
+  } catch (error) {
+    return error;
+  }
+};
+
+const updateRecord = async (table, valueOption, whereOptions) => {
+  try {
+    const updateQuery = `UPDATE ${table} SET ${valueOption} WHERE ${whereOptions} RETURNING *`;
+    const { rows } = await query(updateQuery);
+    return rows[0];
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteRecord = async (table, id) => {
+  try {
+    const deleteQuery = `DELETE FROM ${table} WHERE id = ${id} RETURNING *`;
+    const { rows } = await query(deleteQuery);
+    return rows[0];
+  } catch (error) {
+    return error;
+  }
+};
+
+export {
+  getAll, getById, getAllByOption, getSelectedByOption, updateRecord, insertRecord, deleteRecord
+};
